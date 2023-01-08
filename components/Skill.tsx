cbe303cf5e7c110b7cfeb85e1975d003324fce72
zyfers <himanshusharma7296@gmail.com>
2023-01-08 03:43:04 -0700

    removed skill proficiency

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -27,11 +27,11 @@ export default function Skill({ skill }: Props) {
             className='rounded-full bg-white border border-gray-500 object-cover w-16 h-16 md:w-16 md:h-16 xl:w-24 xl:h-24 filter group-hover:grayscale transition duration-300 ease-in-out'
             alt={skill.alt}
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 md:w-16 md:h-16 xl:w-24 xl:h-24 rounded-full z-0'>
+        {/* <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 md:w-16 md:h-16 xl:w-24 xl:h-24 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-3xl font-bold text-black opacity-100'>{skill.proficiency}%</p>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
